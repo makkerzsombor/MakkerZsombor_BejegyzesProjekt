a3a56393a228473d891fadeba9c68f9cba9be453
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-29 08:24:35 +0200

    2/a completed

--- src/Bejegyzes/Main.java
+++ src/Bejegyzes/Main.java
@@ -1,7 +1,15 @@
 package Bejegyzes;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+    static ArrayList<Bejegyzes> bejegyzesek = new ArrayList<>();
     public static void main(String[] args) {
+        bejegyzesek.add(new Bejegyzes("Kovács István","Ez egy alma fa."));
+        bejegyzesek.add(new Bejegyzes("Nagy Péter","Ez egy körte fa."));
+
 
     }
 }
